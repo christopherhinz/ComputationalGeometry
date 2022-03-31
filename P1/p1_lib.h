e2b8e3f6e37ce42fbbd8f286886079a23904deef
Christopher Hinz <christopher.hinz0@hm.edu>
2022-03-31 21:55:38 +0200

    Fixed line_intersect_check() function

--- P1/p1_lib.h
+++ P1/p1_lib.h
@@ -44,7 +44,16 @@ float ccw(point p, point q, point r){
 }
 
 bool line_intersect_check(line l1, line l2){
+    bool retval = false;
     float ccw_res1 = ccw(l1.p1, l1.p2, l2.p1) * ccw(l1.p1, l1.p2, l2.p2);
     float ccw_res2 = ccw(l2.p1, l2.p2, l1.p1) * ccw(l2.p1, l2.p2, l1.p2);
-    return (ccw_res1 <= 0.0f && ccw_res2 <= 0.0f);
+    if(ccw_res1 <= 0.0f && ccw_res2 <= 0.0f){
+        retval = true;
+        if(ccw_res1 == 0.0f && ccw_res2 == 0.0f){
+            float lambda1 = (l1.p1.x-l2.p1.x)/(l2.p2.x-l2.p1.x);
+            float lambda2 = (l1.p2.x-l2.p1.x)/(l2.p2.x-l2.p1.x);
+            if ((lambda1 < 0 || lambda1 > 1) && (lambda2 < 0 || lambda2 > 1)) retval = false;
+        }
+    }
+    return retval;
 }
