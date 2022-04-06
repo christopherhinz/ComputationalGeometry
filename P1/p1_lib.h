bebde65133c4c2256f880aa96c21a04e99082c59
Tobias Gruber <tobiasgruber@Tobiass-MacBook-Air.local>
2022-04-06 15:31:42 +0200

    bugs fixed

--- P1/p1_lib.h
+++ P1/p1_lib.h
@@ -5,8 +5,8 @@
 
 
 struct point{
-    float x;
-    float y;
+    double x;
+    double y;
 };
 
 struct line{
@@ -15,21 +15,23 @@ struct line{
 };
 
 
-void read_dat(char* filename, unsigned int N, std::vector<float>& v){
+void read_dat(char* filename, unsigned int N, std::vector<double>& v){
     std::ifstream file;
     file.open(filename);
-    float data;
-    for(int i = 0; i < N; ++i){
-        file >> data;
-        v.push_back(data);
+    double k1, k2, k3, k4;
+    while(file >> k1 >> k2 >> k3 >> k4){
+        v.push_back(k1); 
+        v.push_back(k2);
+        v.push_back(k3);
+        v.push_back(k4);
     }
     file.close();
 }
 
 
-void pack_koords(std::vector<float>& source, std::vector<line>& target){
+void pack_koords(std::vector<double>& source, std::vector<line>& target){
     line temp;
-    for(unsigned int i = 0; i < source.size()/4; ++i){
+    for(unsigned int i = 0; i < source.size(); i += 4){
         temp.p1.x = source[0+i];
         temp.p1.y = source[1+i];
         temp.p2.x = source[2+i];
@@ -38,21 +40,22 @@ void pack_koords(std::vector<float>& source, std::vector<line>& target){
     }
 }
 
-float ccw(point p, point q, point r){
-    float res = (p.x*q.y - p.y*q.x)+ (q.x*r.y - q.y*r.x) + (p.y*r.x  - p.x*r.y);
+double ccw(point p, point q, point r){
+    double res = (p.x*q.y - p.y*q.x)+ (q.x*r.y - q.y*r.x) + (p.y*r.x  - p.x*r.y);
     return res;
 }
 
 bool line_intersect_check(line l1, line l2){
     bool retval = false;
-    float ccw_res1 = ccw(l1.p1, l1.p2, l2.p1) * ccw(l1.p1, l1.p2, l2.p2);
-    float ccw_res2 = ccw(l2.p1, l2.p2, l1.p1) * ccw(l2.p1, l2.p2, l1.p2);
+    double ccw_res1 = ccw(l1.p1, l1.p2, l2.p1) * ccw(l1.p1, l1.p2, l2.p2);
+    double ccw_res2 = ccw(l2.p1, l2.p2, l1.p1) * ccw(l2.p1, l2.p2, l1.p2);
     if(ccw_res1 <= 0.0f && ccw_res2 <= 0.0f){
         retval = true;
         if(ccw_res1 == 0.0f && ccw_res2 == 0.0f){
-            float lambda1 = (l1.p1.x-l2.p1.x)/(l2.p2.x-l2.p1.x);
-            float lambda2 = (l1.p2.x-l2.p1.x)/(l2.p2.x-l2.p1.x);
-            if ((lambda1 < 0 || lambda1 > 1) && (lambda2 < 0 || lambda2 > 1)) retval = false;
+            double lambda1 = (l1.p1.x-l2.p1.x)/(l2.p2.x-l2.p1.x);
+            double lambda2 = (l1.p2.x-l2.p1.x)/(l2.p2.x-l2.p1.x);
+            if ((lambda1 < 0 || lambda1 > 1) && (lambda2 < 0 || lambda2 > 1)) 
+                retval = false;
         }
     }
     return retval;
--- P1/strecken.cpp
+++ P1/strecken.cpp
@@ -4,40 +4,42 @@
 #include <chrono>
 
 void print_line(line l1){
-    std::cout << "strecke: (" << l1.p1.x << " " << l1.p1.y << "), " <<  "( " << l1.p2.x << " " << l1.p2.y << ")";
+    std::cout << "strecke: (" << l1.p1.x << " " << l1.p1.y << "), " <<  "(" << l1.p2.x << " " << l1.p2.y << ")";
 } 
 
 int main(){
     auto start = std::chrono::steady_clock::now();
 
 
-    std::vector<float> vec;
+    std::vector<double> vec;
     std::vector<line> lines_vec;
     read_dat((char*)"../strecken/s_1000_1.dat", 1000, vec);
     pack_koords(vec, lines_vec);
 
+    /*for(auto x : lines_vec){
+        print_line(x);
+        std::cout << "\n";
+    }*/
 
     int intersect_counter = 0;
     for(unsigned int i = 0; i < lines_vec.size(); ++i){
-        for(unsigned int j = 0; j < lines_vec.size(); ++j){
+        for(unsigned int j = i; j < lines_vec.size(); ++j){
             if(i!=j){
                 if(line_intersect_check(lines_vec[i], lines_vec[j])){
                     ++intersect_counter;
-                    if(intersect_counter >= 4400){
-                        print_line(lines_vec[i]);
-                        std::cout << " -> ";
-                        print_line(lines_vec[j]);
-                        std::cout << "\n";
-                    }
+                    print_line(lines_vec[i]);
+                    std::cout << " <-> ";
+                    print_line(lines_vec[j]);
+                    std::cout << "\n";
                 }
             }
         }
     }
-    std::cout << "Strecken insgesamt: " << vec.size() << "\n" << "Schnitte zweier Strecken: " << intersect_counter << "\n";
+    std::cout << "Strecken insgesamt: " << lines_vec.size() << "\n" << "Schnitte zweier Strecken: " << intersect_counter << "\n";
 
     auto end = std::chrono::steady_clock::now();     
     std::cout << "Runtime: " 
-    << (float)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()/1000 
+    << (double)std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()/1000 
         + std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
 
     return 0;
