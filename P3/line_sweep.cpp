2f0d88426ecf0ccd6ba191da133f6b9d42e6a441
Tobias Gruber <tobiasgruber@ecdf8ddc-3681-4f19-a7b4-b58d5ac7b962.fritz.box>
2022-06-07 21:55:06 +0200

    P3: weitergearbeitet

--- P3/line_sweep.cpp
+++ P3/line_sweep.cpp
@@ -4,6 +4,15 @@
 #include <iostream>
 #include <iomanip>
 
+int while_count = 0;
+void log_messages(sweep_line& sl, point& E){
+    (++while_count, std::cout << "======  Durchlauf " << while_count << "  ======\n");
+    //std::cout << "Current Event: " << (E.pt == 0 ? "BEG " : E.pt == 1 ? "END " : "SEC ") << E.lineID << "\n";
+    sl.print_event_queue();
+    sl.print_segment_queue();
+}
+
+typedef unsigned long long uintLL;
 
 int main(){
 
@@ -29,34 +38,34 @@ int main(){
         }
 
 
-        int intersect_count = 0;
+        uintLL intersect_count = 0;
         std::vector<point> intersec_list;
 
         // sortieren nach x Werten, falls x Werte gleich sortieren nach y Werten
         sl.sort_event_queue();
-        //sl.init_segment_queue();
 
-    
         while(sl.event_queue.size() != 0){
-            sl.print_event_queue();
+
             point E = sl.event_queue.back();
+
+            log_messages(sl, E);
             int current_index = 0;
 
             // BEGIN OF LINE
             if(E.pt == point_type::BEG){
                 // muss mit y-Wert passend einsortiert werden
                 if(sl.segment_queue.size() == 0){
-                    sl.segment_queue.push_back(E);
+                    sl.segment_queue.push_back( segment{.lineID = E.lineID, .y_beg = E.y} );
                 }
                 else{
-                    int size = sl.segment_queue.size();
+                    uintLL size = sl.segment_queue.size();
                     while(current_index < size+1){
-                        if(sl.segment_queue[current_index].y < E.y){
-                            sl.segment_queue.insert(sl.segment_queue.begin()+current_index, E);
+                        if(sl.segment_queue[current_index].y_beg < E.y){
+                            sl.segment_queue.insert( sl.segment_queue.begin()+current_index, segment{.lineID = E.lineID, .y_beg = E.y} );
                             break;
                         }
                         if(current_index == size){
-                            sl.segment_queue.push_back(E);
+                            sl.segment_queue.push_back( segment{.lineID = E.lineID, .y_beg = E.y} );
                             break;
                         }
                         ++current_index;
@@ -67,7 +76,7 @@ int main(){
                 if(current_index < sl.segment_queue.size()-1){
                     auto above_intersect = line_intersect_check(lines_by_index[sl.segment_queue[current_index  ].lineID-1], 
                                                                 lines_by_index[sl.segment_queue[current_index+1].lineID-1]);
-                    if(above_intersect.first){
+                    if(above_intersect.first && !point_in_event_queue(above_intersect.second, sl.event_queue)){
                         sl.event_queue.push_back(above_intersect.second);
                         sl.sort_event_queue();
                     }
@@ -77,7 +86,7 @@ int main(){
                 if(current_index > 0){
                     auto below_intersect = line_intersect_check(lines_by_index[sl.segment_queue[current_index  ].lineID-1], 
                                                                 lines_by_index[sl.segment_queue[current_index-1].lineID-1]);
-                    if(below_intersect.first){
+                    if(below_intersect.first && !point_in_event_queue(below_intersect.second, sl.event_queue)){
                         sl.event_queue.push_back(below_intersect.second);
                         sl.sort_event_queue();
                     }
@@ -87,8 +96,8 @@ int main(){
             // END OF LINE
             else if (E.pt == point_type::END){
 
-                for(int i = 0; i < sl.segment_queue.size(); ++i){
-                    if(sl.segment_queue[i].lineID == E.lineID && sl.segment_queue[i].pt == E.pt)
+                for(uintLL i = 0; i < sl.segment_queue.size(); ++i){
+                    if(sl.segment_queue[i].lineID == E.lineID)
                         current_index = i;
                 }
 
@@ -96,23 +105,63 @@ int main(){
                 if(current_index > 0 && current_index < sl.segment_queue.size()-1){
                     auto above_intersect = line_intersect_check(lines_by_index[sl.segment_queue[current_index-1].lineID-1], 
                                                                 lines_by_index[sl.segment_queue[current_index+1].lineID-1]);
-                    if(above_intersect.first){
+                    if(above_intersect.first && !point_in_event_queue(above_intersect.second, sl.event_queue)){
                         sl.event_queue.push_back(above_intersect.second);
                         sl.sort_event_queue();
                     }
                 }
-                sl.event_queue.erase(sl.event_queue.begin()+current_index);                
+                //sl.event_queue.erase(sl.event_queue.begin()+current_index);        
+                sl.erase_segment(E.lineID);        
             } 
 
             // INTERSECTION
             else if (E.pt == point_type::SEC) {
                 intersec_list.push_back(E);
+                uintLL i_line1, i_line2;
+                for(uintLL i = 0; i < sl.segment_queue.size(); ++i){
+                    if(sl.segment_queue[i].lineID == lines_by_index[E.line1].p1.lineID) 
+                        i_line1 = i;
+                    else if(sl.segment_queue[i].lineID == lines_by_index[E.line2].p1.lineID) 
+                        i_line2 = i;
+                }
+                std::cout << "Segments to swap: " << i_line1 << " " << i_line2 << std::endl;
+                swap_segments(sl.segment_queue[i_line1], sl.segment_queue[i_line2]);
+
+                               
+                
+                // check segment above recently added segment
+                if(i_line1 < sl.segment_queue.size()-1){
+                    auto above_intersect = line_intersect_check(lines_by_index[sl.segment_queue[i_line1  ].lineID-1], 
+                                                                lines_by_index[sl.segment_queue[i_line1+1].lineID-1]);
+                    if(above_intersect.first && !point_in_event_queue(above_intersect.second, sl.event_queue)){
+                        std::cout << "added1: " << above_intersect.second << "\n";
+                        sl.event_queue.push_back(above_intersect.second);
+                        sl.sort_event_queue();
+                    }
+                }
+
+                // check segment below recently added segment
+                if(i_line2 > 0 && sl.segment_queue.size() >= 2){
+                    auto below_intersect = line_intersect_check(lines_by_index[sl.segment_queue[i_line2  ].lineID-1], 
+                                                                lines_by_index[sl.segment_queue[i_line2-1].lineID-1]);
+                    if(below_intersect.first && !point_in_event_queue(below_intersect.second, sl.event_queue)){
+                        std::cout << "added2: " << below_intersect.second << "\n";
+                        sl.event_queue.push_back(below_intersect.second);
+                        sl.sort_event_queue();
+                    }
+                } 
+                
             }
 
             for(int i = 0; i < sl.event_queue.size(); ++i){
-                if(E.pt == sl.event_queue[i].pt && E.lineID == sl.event_queue[i].lineID)
+                if(E.pt == sl.event_queue[i].pt && E.lineID == sl.event_queue[i].lineID){
+                    std::cout << "removed from event_queue: " << E << std::endl;
                     sl.event_queue.erase(sl.event_queue.begin()+i);
+                }
             }
+
+            //if(while_count == 7)
+            //    break;
 
         } // while()
 
--- P3/p3_lib.h
+++ P3/p3_lib.h
@@ -6,16 +6,26 @@
 
 enum point_type { BEG = 0, END = 1, SEC = 2};
 
+
 struct point{
     double x;
     double y;
     point_type pt;
-    std::size_t lineID;
+    std::size_t lineID = 0;
+    std::size_t line1 = 0;
+    std::size_t line2 = 0;
 };
+bool operator==(point& p1, point& p2){
+    return p1.x == p2.x && p1.y == p2.y && p1.pt == p2.pt && p1.lineID == p2.lineID;
+}
+bool operator!=(point& p1, point& p2){
+    return !(p1==p2);
+}
 std::ostream& operator<<(std::ostream& os, point& p){
-    os << "(" << p.x << "," << p.y << ")";
+    os << "pt=" << p.pt << ", lineID=" << p.lineID << ", koords=(" << p.x << "," << p.y << ")";
     return os;
 }
+
 
 struct line{
     point p1;
@@ -26,26 +36,52 @@ std::ostream& operator<<(std::ostream& os, line& l){
     return os;
 }
 
+struct segment{
+    std::size_t lineID;
+    double y_beg;
+};
+void swap_segments(segment& s1, segment& s2){
+    segment s_tmp = {.lineID = s1.lineID, .y_beg = s1.y_beg};
+    s1 = {.lineID = s2.lineID, .y_beg = s2.y_beg};
+    s2 = {.lineID = s_tmp.lineID, .y_beg = s_tmp.y_beg};
+}
+
 struct sweep_line{
     point current_event;
-    std::vector<point> segment_queue;
+    std::vector<segment> segment_queue;
     std::vector<point> event_queue;
     void sort_event_queue(){
         std::sort(event_queue.begin(), event_queue.end(), [](point& p1, point& p2){return p1.x == p2.x ? p1.y > p2.y : p1.x > p2.x;});
     }
-    void init_segment_queue(){
-        segment_queue.push_back(event_queue.back());
-    }
     void print_event_queue(){
         std::cout << "Event queue:\n";
-        for(auto& elem : event_queue)
-            std::cout << "\t lineID " << elem.lineID << ": "<< elem << "\n";
+        for(auto& elem : event_queue){
+            if(elem.pt == BEG || elem.pt == END)
+                std::cout << "\t" << (elem.pt == 0 ? "BEG" : "END") << elem.lineID << ": "<< elem << "\n";
+            else
+                std::cout << "\tSEC " << elem.line1 << "," << elem.line2 << ": " << elem << "\n";
+        }
         std::cout << "\n";
+    }
+    void print_segment_queue(){
+        std::cout << "Segment queue:\n";
+        for(auto& elem : segment_queue)
+            std::cout   << "\tlineID" << elem.lineID << "\n";
+        std::cout << "\n";
+    }
+    void erase_segment(std::size_t lineID){
+        for(std::size_t i = 0; i < segment_queue.size(); ++i){
+            if(segment_queue[i].lineID == lineID){
+                segment_queue.erase(segment_queue.begin()+1);
+                std::cout << "successfully erased segment: " << lineID << std::endl;
+                break;
+            }
+        }
     }
 };
 
 void read_dat(std::string filename, std::vector<point>& target){
-    point pb, pe;
+    point p_beg, p_end;
     std::size_t countID = 1;
     std::ifstream file;
     file.open(filename.c_str());
@@ -55,16 +91,16 @@ void read_dat(std::string filename, std::vector<point>& target){
     }
     while(file >> k1 >> k2 >> k3 >> k4){
         if(k1 < k3){
-            pb = { k1, k2, BEG, countID};
-            pe = { k3, k4, END, countID};
+            p_beg = { .x = k1, .y = k2, .pt = BEG, .lineID = countID};
+            p_end = { .x = k3, .y = k4, .pt = END, .lineID = countID};
         }
         else{
-            pb = { k1, k2, END, countID};
-            pe = { k3, k4, BEG, countID};
+            p_beg = { .x = k1, .y = k2, .pt = END, .lineID = countID};
+            p_end = { .x = k3, .y = k4, .pt = BEG, .lineID = countID};
         }
         ++countID;
-        target.push_back(pb);
-        target.push_back(pe);
+        target.push_back(p_beg);
+        target.push_back(p_end);
     }
     file.close();
 }
@@ -81,7 +117,7 @@ point calc_intersect_point(line _l1, line _l2){
     l2.p1 = _l2.p1;
     l2.p2 = {.x = _l2.p2.x - _l2.p1.x, .y = _l2.p2.y - _l2.p1.y};
     double lambda2 = (l1.p1.y + l2.p1.x*l1.p2.y/l1.p2.x - l2.p1.y - l1.p1.x*l1.p2.y/l1.p2.x)/(l2.p2.y - l2.p2.x*l1.p2.y/l1.p2.x);
-    point intersect_point = {.x = l2.p1.x + lambda2 * l2.p2.x, .y = l2.p1.y + lambda2 * l2.p2.y, .pt = SEC};
+    point intersect_point = {.x = l2.p1.x + lambda2 * l2.p2.x, .y = l2.p1.y + lambda2 * l2.p2.y, .pt = SEC, .line1 = _l1.p1.lineID, .line2 = _l2.p1.lineID};
     return intersect_point;
 }
 
@@ -106,23 +142,12 @@ std::pair<bool, point> line_intersect_check(line l1, line l2){
     return std::pair<bool, point>(retval, p_intersect);
 }
 
-/*
-void TreatLeftEndpoint(point& E, sweep_line& sl){
-    std::size_t segE = E.lineID;
-    sl.segment_queue.push_back(segE);
 
-    std::size_t segA ;
+bool point_in_event_queue(point& p, std::vector<point>& event_queue){
+    bool check = false;
+    for(auto& point : event_queue){
+        if(point == p)
+            check = true;
+    }
+    return check;
 }
-
-void TreatRightEndpoint(point& E, sweep_line& sw){
-    return;
-}
-
-void TreatIntersection(point& E, sweep_line& sw){
-    return;
-}
-
-
-void sort_xvals(){
-    return;
-}*/
